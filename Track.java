8e9af94114de85dba6be84f730e3185493d90698
BritishTeapot <f.kovalyoff@gmail.com>
2023-12-04 15:05:12 +0100

    Made Tracks comparable.

--- Track.java
+++ Track.java
@@ -1,4 +1,4 @@
-
+import java.util.Objects;
 
 public class Track {
     String trackLabel;
@@ -10,5 +10,13 @@ public class Track {
     @Override
     public String toString() {
         return trackLabel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Track track = (Track) o;
+        return Objects.equals(trackLabel, track.trackLabel);
     }
 }
