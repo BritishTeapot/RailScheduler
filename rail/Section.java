c7d1929b0534ecda362b7a56d203ce51379d7910
BritishTeapot <f.kovalyoff@gmail.com>
2023-12-04 20:46:23 +0100

    Added input method for Section

--- rail/Section.java
+++ rail/Section.java
@@ -1,6 +1,8 @@
 package rail;
 
 import java.util.Set;
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class Section {
     Set<Track> tracks;
@@ -18,5 +20,24 @@ public class Section {
 
     public boolean contains(Track track) {
         return tracks.contains(track);
+    }
+
+    public static Section inputSection(Scanner scanner, boolean provideFeedback) {
+        Set<Track> tracks = new HashSet<>();
+
+        if (provideFeedback) {
+            System.out.println("Enter number of tracks in the Section:");
+        }
+        int numTracks = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
+
+        if (provideFeedback) {
+            System.out.println("Enter tracks' labels:");
+        }
+        for (int i = 0; i < numTracks; i++) {
+            tracks.add(Track.inputTrack(scanner, false));
+        }
+
+        return new Section(tracks);
     }
 }
