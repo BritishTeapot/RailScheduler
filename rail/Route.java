fb3decbe22a7fe94562e01b94d7cb12d0de133ae
BritishTeapot <f.kovalyoff@gmail.com>
2023-12-04 20:46:41 +0100

    Added input method for Route

--- rail/Route.java
+++ rail/Route.java
@@ -1,8 +1,10 @@
 package rail;
 
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
 
-class Route {
+public class Route {
     List<Track> path;
 
     public Route(List<Track> path) {
@@ -11,8 +13,31 @@ class Route {
 
     @Override
     public String toString() {
-        return "rail.Route{" +
+        return "Route{" +
                 "path=" + path +
                 '}';
+    }
+
+    public List<Track> getPath() {
+        return path;
+    }
+
+    public static Route inputRoute(Scanner scanner, boolean provideFeedback) {
+        List<Track> path = new ArrayList<>();
+
+        if (provideFeedback) {
+            System.out.println("Enter number of tracks in the Route:");
+        }
+        int numTracks = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
+
+        if (provideFeedback) {
+            System.out.println("Enter your path:");
+        }
+        for (int i = 0; i < numTracks; i++) {
+            path.add(Track.inputTrack(scanner, false));
+        }
+
+        return new Route(path);
     }
 }
